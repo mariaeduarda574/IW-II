faad43b4c99cb4c87c45672579a50d1b46065028
Maria Eduarda <m64810816@gmail.com>
2026-03-04 08:08:18 -0300

    Delete aula_03 directory

--- aula_03/exemploalert.js
+++
@@ -1,8 +0,0 @@
-alert("Bem vindo ao nosso site!!!");
-
-let resposta = confirm("Você deseja continuar ?");
-if(resposta) {
-    alert("Você escolheu continuar.");
-}else{
-    alert("Você cancelou a ação.")
-}
--- aula_03/exemploprompt.js
+++
@@ -1,2 +0,0 @@
-let nome = prompt ("Qual o seu nome?");
-alert("Olá, " +nome+ "!");
